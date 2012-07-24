5794a6b6db49672d379bfa43b232e2641f858440
Dave Hunt <dave.hunt@gmail.com>
2012-07-24 23:06:42 +0100

    On second thoughts, read everything from the configuration file.

--- flynnid/flynnid.py
+++ flynnid/flynnid.py
@@ -6,91 +6,35 @@
 
 from optparse import OptionParser
 import json
+import os
+import sys
 import urllib2
 
 def main ():
-    parser = OptionParser(version='%prog 0.2')
+    usage = "Usage: %prog [options] config"
+    parser = OptionParser(usage=usage, version='%prog 0.2')
     parser.set_defaults(verbose=False)
     parser.add_option('-v', '--verbose',
                       action='store_true',
                       help='increase verbosity')
-    parser.add_option('--hubhost',
-                      action='store',
-                      type='string',
-                      metavar='str',
-                      dest='hub_host',
-                      default='localhost',
-                      help='host selenium grid is listening on [default: %default]')
-    parser.add_option('--hubport',
-                      action='store',
-                      type='int',
-                      metavar='num',
-                      dest='hub_port',
-                      default=4444,
-                      help='port selenium grid is listening on [default: %default]')
-    parser.add_option('--nodeconfig',
-                      action='store',
-                      type='string',
-                      metavar='path',
-                      dest='node_config',
-                      help='configuration file for nodes to register.')
-    parser.add_option('--nodehost',
-                      action='store',
-                      type='string',
-                      metavar='str',
-                      dest='node_host',
-                      default='localhost',
-                      help='host selenium node is listening on [default: %default]')
-    parser.add_option('--nodeport',
-                      action='store',
-                      type='int',
-                      metavar='num',
-                      dest='node_port',
-                      default=5555,
-                      help='port selenium node is listening on [default: %default]')
-    parser.add_option('--browsername',
-                      action='store',
-                      type='str',
-                      metavar='str',
-                      dest='browser_name',
-                      help='name of browser available on node')
-    parser.add_option('--browserver',
-                      action='store',
-                      type='str',
-                      metavar='str',
-                      dest='browser_version',
-                      help='version of browser available on node')
-    parser.add_option('--platform',
-                      action='store',
-                      type='str',
-                      metavar='str',
-                      help='platform of node')
     (options, args) = parser.parse_args()
-    
-    url = 'http://%s:%i/grid/register' % (options.hub_host, options.hub_port)
 
-    nodes = []
-    if options.node_config:
-        f = open(options.node_config)
-        nodes.extend(json.load(f))
-    else:
-        if not options.browser_name:
-            raise Exception('--browsername must be specified!')
-        if not options.browser_version:
-            raise Exception('--browserver must be specified!')
-        if not options.platform:
-            raise Exception('--platform must be specified!')
-        nodes.append({
-            'host':options.node_host,
-            'port':options.node_port,
-            'browser':{
-                'name':options.browser_name,
-                'version':options.browser_version
-            },
-            'platform':options.platform
-        })
+    if len(args) < 1:
+        sys.exit('ERROR: Configuration file not specified!')
 
-    for node in nodes:
+    if not os.path.exists(args[0]):
+        sys.exit('ERROR: Configuration file %s not found!' % args[0])
+
+    config = json.load(open(args[0]))
+
+    hub_host = config['hub']['host']
+    hub_port = config['hub']['port']
+
+    url = 'http://%s:%i/grid/register' % (hub_host, hub_port)
+
+    for node in config['nodes']:
+        node_host = node['host']
+        node_port = node['port']
         data = {
             'class': 'org.openqa.grid.common.RegistrationRequest',
             'capabilities': [{
@@ -100,18 +44,18 @@ def main ():
                 'platform': node['platform'],
                 'maxInstances': 1}],
             'configuration': {
-                'port': node['port'],
-                'host': node['host'],
+                'port': node_port,
+                'host': node_host,
                 'proxy': 'org.openqa.grid.selenium.proxy.DefaultRemoteProxy',
                 'register': True,
                 'registerCycle': 5000,
-                'hubHost': options.hub_host,
-                'hub': 'http://%s:%i/grid/register' % (options.hub_host, options.hub_port),
-                'url': 'http://%s:%i' % (node['host'], node['port']),
-                'remoteHost': 'http://%s:%i' % (node['host'], node['port']),
+                'hubHost': config['hub']['host'],
+                'hub': 'http://%s:%i/grid/register' % (hub_host, hub_port),
+                'url': 'http://%s:%i' % (node_host, node_port),
+                'remoteHost': 'http://%s:%i' % (node_host, node_port),
                 'maxSession': 1,
                 'role': 'node',
-                'hubPort': options.hub_port}}
+                'hubPort': hub_port}}
         data = json.dumps(data)
         if options.verbose:
             print 'Using JSON request: %s' % data
